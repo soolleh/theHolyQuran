c79061972a6e857a04d6abc6bee21bde7889ded6
Soleh <soleh.asime@gmail.com>
2017-07-13 23:50:52 +0530

    made ui view

--- pages/logIn.php
+++ pages/logIn.php
@@ -8,12 +8,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- *****Style Sheets***** -->
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/logIn.css">
 	<!-- *****Scripts***** -->
-	<script src="js/jquery-2.1.4.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/script.js"></script>    
+	<script src="../js/jquery-2.1.4.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/script.js"></script>    
 
 </head>
 <body>
@@ -23,16 +23,49 @@
 	<div class="container-fluid header-outer-div">
 		<div class="container header-inner-div">
 			<div class="logo">
-				<img class="logo-img img-responsive" src="images/logo1-lg.png">
+				<img class="logo-img img-responsive" src="../images/logo1.png" width="90" height="90">
 					<span class="arabic" dir="rtl" lang="ar">القران الکریم</span>
-					<span class="account">
+					<!-- <span class="account">
 						<a href="pages/signUp.php">Sign-Up</a> 
-						<a href="test.html">Log In</a> 
-					</span>
+						<a href="pages/logIn.php">Log In</a> 
+					</span> -->
 			</div>
 		</div>
 	</div>
 </header>
+<div class="container-fluid">
+	<div class="container">
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4 well">
+			<form role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="loginform">
+				<fieldset>
+					<legend>Login</legend>
+					
+					<div class="form-group">
+						<label for="name">Email</label>
+						<input type="text" name="email" placeholder="Your Email" required class="form-control" />
+					</div>
+
+					<div class="form-group">
+						<label for="name">Password</label>
+						<input type="password" name="password" placeholder="Your Password" required class="form-control" />
+					</div>
+
+					<div class="form-group">
+						<input type="submit" name="login" value="Login" class="btn btn-primary" />
+					</div>
+				</fieldset>
+			</form>
+			<span class="text-danger"><?php if (isset($errormsg)) { echo $errormsg; } ?></span>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4 text-center">	
+		New User? <a href="register.php">Sign Up Here</a>
+		</div>
+	</div>
+</div>
+</div>
 <!-- <div class="container-fluid">
 	<div class="container">
 		<div class="panel panel-primary">
